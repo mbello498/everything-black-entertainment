831d6451b5008e7a75d5ae2b2ef9c4f7f8c0a190
Maya Bello <maya_bello@intuit.com>
2020-03-20 12:34:06 -0700

    Added API call + styling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import { Header } from './components/Header';
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-import { SearchProvider, Results, SearchBox } from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
-import "@elastic/react-search-ui-views/lib/styles/styles.css";
-
-const connector = new AppSearchAPIConnector({
-  searchKey: "search-371auk61r2bwqtdzocdgutmg",
-  engineName: "search-ui-examples",
-  hostIdentifier: "host-2376rb"
-});
+import { Header } from './components/Header/Header';
+import { SearchBar } from './components/SearchBar/SearchBar';
+import getMovies from './services/movieService';
+import Card from 'react-bootstrap/Card'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
+
+  const [input, setInput] = useState("");
+  const [movieResults, setMovieResults] = useState([]);
+
+  const handleInput = function(e) {
+    setInput(e.target.value)
+  }
+
+  const handleSearch = async function(e) {
+    e.preventDefault();
+    let response = await getMovies();
+    response = [response]
+    setMovieResults(response)
+  }
+
+  const handleGenreFilter = (movie) => {
+    let i;
+    for(i = 0; i < movie.genres.length; i++) {
+      if(movie.genres[i].toLowerCase() === input.toLowerCase()) {
+        return (
+          <Card className="card">
+            <Card.Img variant="top" src={"https://image.tmdb.org/t/p/w500" + movie.poster} />
+            <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.overview}</Card.Text>
+            </Card.Body>
+          </Card>
+        )
+      }
+    }
+  }
+
+  const Movies = movieResults && movieResults.map((movieArray) => {
+    return movieArray.map((movie) => {
+      return handleGenreFilter(movie);
+    })
+  })
+
   return (
     <div>
       <Header />
-      <SearchProvider
-        config={{
-          apiConnector: connector
-        }}
-      >
-        <div className="App">
-          <Layout
-            header={<SearchBox />}
-            bodyContent={<Results titleField="title" urlField="nps_link" />}
-          />
-        </div>
-      </SearchProvider>
+      <SearchBar handleInput={handleInput} handleSearch={handleSearch} />
+      <div className="moviesContainer">{Movies}</div>
     </div>
   );
 }

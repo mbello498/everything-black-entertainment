b8c68e87a3f61c5973c9b0a33dbbbba24ec01c94
Maya Bello <maya_bello@intuit.com>
2020-03-23 20:05:54 -0700

    test

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Header.scss";
 
 export const Header = () => {
+    //test
     return (
         <h1 className="Header">Everything Black Entertainment</h1>
     );
